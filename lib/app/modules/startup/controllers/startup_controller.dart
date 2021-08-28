17ae0a549f2a8ba0d826d99d7da092c28f48ccde
Smile Garg <smlgarg123@gmail.com>
2021-08-29 02:12:53 +0530

    check for password on startup screen

--- lib/app/modules/startup/controllers/startup_controller.dart
+++ lib/app/modules/startup/controllers/startup_controller.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:password_manager/app/core/values/strings.dart';
 import 'package:password_manager/app/data/services/database_service/database_service.dart';
 import 'package:password_manager/app/data/services/secure_key_service.dart';
+import 'package:password_manager/app/modules/register/views/set_password_view.dart';
 import 'package:password_manager/app/routes/app_pages.dart';
 
 class StartupController extends GetxController {
@@ -18,12 +19,17 @@ class StartupController extends GetxController {
 
   void validate() async {
     final service = Get.find<SecureKeyService>();
-    bool hasKey = await service.hasKey(passCode);
-    if (hasKey) {
-      Get.toNamed(Routes.LOGIN);
-    } else {
-      Get.toNamed(Routes.REGISTER);
+    bool hasPassCode = await service.hasKey(passCode);
+    bool hasPassword = await service.hasKey(passwordKey);
+
+    if (hasPassCode) {
+      if (hasPassword) {
+        return Get.offNamed(Routes.LOGIN);
+      } else {
+        return Get.offAll(() => SetPasswordView());
+      }
     }
+    return Get.offNamed(Routes.REGISTER);
   }
 
   Future<void> initDb() async {
