8ff7c5eb3156ef8ebd759502fe456d85f6c759f7
Smile Garg <smlgarg123@gmail.com>
2021-08-31 01:15:52 +0530

    some android gradle stuff changed

--- android/app/src/main/kotlin/com/samgarg/password_manager/MainActivity.kt
+++ android/app/src/main/kotlin/com/samgarg/password_manager/MainActivity.kt
@@ -1,6 +1,25 @@
 package com.samgarg.password_manager
 
+import android.content.Context
+import android.content.Intent
+import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "password_manager").setMethodCallHandler { call, result ->
+            if (call.method == "getDocument") {
+                val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE).apply {
+                    flags = Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+                }
+                startActivityForResult(intent,2)
+                Log.d(intent.dataString,"Data")
+            } else {
+                result.notImplemented();
+            }
+        }
+    }
 }
