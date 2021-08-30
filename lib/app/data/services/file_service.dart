b4a07b20e5ca889d738c32351916e259c487436e
Smile Garg <smlgarg123@gmail.com>
2021-08-31 01:15:39 +0530

    file service file create

---
+++ lib/app/data/services/file_service.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:get/get.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
+
+class FileService extends GetxService {
+  Future<void> shareFile(String path) async {
+    await Share.shareFiles([path]);
+  }
+
+  Future<PlatformFile?> pickFile() async {
+    final pickedFile = await FilePicker.platform.pickFiles(
+      allowedExtensions: ["json"],
+    );
+    if (pickedFile != null) {
+      return pickedFile.files.first;
+    } else {
+      return null;
+    }
+  }
+
+  Future<File> createTextFile(String text, String path) async {
+    final dir = await getExternalStorageDirectory();
+    final fullPath = join(dir!.path, path);
+    return await File(fullPath).writeAsString(text);
+  }
+
+  Future<String> readTextFile(String path) async {
+    return await File(path).readAsString();
+  }
+
+  Future decodeJsonFile(String path) async {
+    final file = await this.readTextFile(path);
+    return JsonDecoder().convert(file);
+  }
+}
