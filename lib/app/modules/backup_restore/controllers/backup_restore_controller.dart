c7f404da4cb51c3546c66099bae1dfb0ecd3bc58
Smile Garg <smlgarg123@gmail.com>
2021-08-31 01:16:05 +0530

    some progress on backup

--- lib/app/modules/backup_restore/controllers/backup_restore_controller.dart
+++ lib/app/modules/backup_restore/controllers/backup_restore_controller.dart
@@ -1,13 +1,30 @@
 import 'package:get/get.dart';
-import 'package:password_manager/app/core/values/strings.dart';
+import 'package:password_manager/app/core/utils/helpers.dart';
+import 'package:password_manager/app/data/models/backup_model.dart';
 import 'package:password_manager/app/data/models/password_model.dart';
-import 'package:password_manager/app/data/services/shared_pref_service.dart';
+import 'package:password_manager/app/data/services/file_service.dart';
 import 'package:password_manager/app/modules/home/controllers/home_controller.dart';
 
+////TODOs For Future Version;
+// enum BackupOptions {
+//   FullEncrypted,
+//   PasswordsEncrypted,
+//   UnEncrypted,
+// }
+
 class BackupRestoreController extends GetxController {
-  late final int? lastBackupTime;
+  // late final int? lastBackupTime;
   late final int? totalPasswords;
   late final List<Password> _passwords;
+  late final bool showBackupButton;
+
+  bool backupButtonLoading = false;
+  bool restoreButtonLoading = false;
+
+  final String backupButtonId = "Backup Button";
+  final String restoreButtonId = "Restore Button";
+
+  final noPassErrorMsg = "You Do Not Have Any Passwords Saved!";
 
   @override
   void onInit() async {
@@ -16,10 +33,45 @@ class BackupRestoreController extends GetxController {
   }
 
   Future<void> initComponents() async {
-    lastBackupTime =
-        await Get.find<SharedPrefService>().storage.getInt(lastBackup);
+    // lastBackupTime = await Get.find<SharedPrefService>().storage.getInt(
+    //       lastBackup,
+    //     );
 
     _passwords = Get.find<HomeController>().passwords;
     totalPasswords = _passwords.length;
+    showBackupButton = totalPasswords != 0;
+  }
+
+  void toggleBackupLoading(bool value) {
+    backupButtonLoading = value;
+    update([backupButtonId]);
+  }
+
+  void toggleRestoreLoading(bool value) {
+    restoreButtonLoading = value;
+    update([restoreButtonId]);
+  }
+
+  void performBackup() async {
+    if (totalPasswords == 0) {
+      errorSnackbar(noPassErrorMsg);
+      return;
+    }
+    toggleBackupLoading(true);
+    await _backup();
+    toggleBackupLoading(false);
+  }
+
+  void performRestore() {}
+
+  Future _backup() async {
+    Backup backup = Backup(
+      dateCreated: DateTime.now().millisecondsSinceEpoch,
+      passwords: _passwords,
+    );
+    final string = backup.toString();
+    final fileService = Get.find<FileService>();
+    final file = await fileService.createTextFile(string, "backup.json");
+    await fileService.shareFile(file.path);
   }
 }
--- lib/app/modules/backup_restore/views/backup_restore_view.dart
+++ lib/app/modules/backup_restore/views/backup_restore_view.dart
@@ -1,8 +1,11 @@
+import 'package:custom_utils/spacing_utils.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:password_manager/app/core/theme/app_theme.dart';
 import 'package:password_manager/app/global_widgets/app_bar.dart';
-
+import 'package:password_manager/app/global_widgets/buttons.dart';
+import 'package:velocity_x/velocity_x.dart';
 import '../controllers/backup_restore_controller.dart';
 
 class BackupRestoreView extends GetView<BackupRestoreController> {
@@ -10,9 +13,69 @@ class BackupRestoreView extends GetView<BackupRestoreController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(heading: "Backup & Restore"),
-      body: ListView(
-        children: [],
+      body: Padding(
+        padding: PaddingTheme.sidePaddingM,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                verSpacing20,
+                "Total Passwords: ${controller.totalPasswords}"
+                    .text
+                    .bold
+                    .size(18)
+                    .make(),
+                verSpacing20,
+                GetBuilder(
+                  id: controller.backupButtonId,
+                  init: controller,
+                  builder: (_) {
+                    return CustomButton(
+                      "Backup",
+                      onTap: controller.performBackup,
+                    );
+                  },
+                ),
+                verSpacing20,
+                RestoreSection(),
+                verSpacing20,
+                GetBuilder(
+                  init: controller,
+                  id: controller.restoreButtonId,
+                  builder: (_) {
+                    return CustomButton(
+                      "Restore",
+                      onTap: controller.performRestore,
+                    );
+                  },
+                ),
+                verSpacing20,
+              ],
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                "Note".text.bold.size(16).make(),
+                verSpacing4,
+                "Backup Will Generate A .json File And Then Prompt You To Share File"
+                    .text
+                    .make(),
+                verSpacing20,
+              ],
+            ),
+          ],
+        ),
       ),
     );
+  }
+}
+
+class RestoreSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
