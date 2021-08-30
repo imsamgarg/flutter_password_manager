225023346e75d3a1773004b407660b62e023eb6a
Smile Garg <smlgarg123@gmail.com>
2021-08-30 16:59:11 +0530

    backup_model generated

---
+++ lib/app/data/models/backup_model.dart
@@ -0,0 +1,27 @@
+import 'package:password_manager/app/data/models/password_model.dart';
+
+class Backup {
+  int? dateCreated;
+  List<Password>? passwords;
+
+  Backup({this.dateCreated, this.passwords});
+
+  Backup.fromJson(Map<String, dynamic> json) {
+    dateCreated = json['date_created'];
+    if (json['passwords'] != null) {
+      passwords = <Password>[];
+      json['passwords'].forEach((v) {
+        passwords?.add(Password.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data['date_created'] = dateCreated;
+    if (passwords != null) {
+      data['passwords'] = passwords?.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
