a299b3f4f015395f5f0ed705006450436430bf30
Smile Garg <smlgarg123@gmail.com>
2021-08-31 01:19:38 +0530

    file service added to startup

--- lib/app/modules/startup/bindings/startup_binding.dart
+++ lib/app/modules/startup/bindings/startup_binding.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 
 import 'package:password_manager/app/data/services/database_service/database_service.dart';
 import 'package:password_manager/app/data/services/encryption_service.dart';
+import 'package:password_manager/app/data/services/file_service.dart';
 import 'package:password_manager/app/data/services/shared_pref_service.dart';
 import 'package:password_manager/app/data/services/secure_key_service.dart';
 
@@ -14,6 +15,7 @@ class StartupBinding extends Bindings {
     Get.put<SharedPrefService>(SharedPrefService());
     Get.put<DatabaseService>(DatabaseService());
     Get.put<EncryptionService>(EncryptionService());
+    Get.put<FileService>(FileService());
     Get.put<StartupController>(StartupController());
   }
 }
