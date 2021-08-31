ba6104118b5fcb0501ee71e3949de45279350a30
Smile Garg <smlgarg123@gmail.com>
2021-09-01 03:16:18 +0530

    file service bug fix

--- lib/app/data/services/file_service.dart
+++ lib/app/data/services/file_service.dart
@@ -14,6 +14,7 @@ class FileService extends GetxService {
 
   Future<PlatformFile?> pickFile() async {
     final pickedFile = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
       allowedExtensions: ["json"],
     );
     if (pickedFile != null) {
