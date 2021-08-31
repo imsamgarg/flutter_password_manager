a5840857a072fa6090b52c59233594e079a709a9
Smile Garg <smlgarg123@gmail.com>
2021-09-01 03:17:15 +0530

    Restore View DOne

--- lib/app/modules/backup_restore/views/restore_view.dart
+++ lib/app/modules/backup_restore/views/restore_view.dart
@@ -1,6 +1,7 @@
 import 'package:custom_utils/spacing_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:password_manager/app/global_widgets/auth_views_widgets.dart';
 import 'package:password_manager/app/global_widgets/buttons.dart';
 import 'package:password_manager/app/modules/backup_restore/controllers/backup_restore_controller.dart';
 import 'package:velocity_x/velocity_x.dart';
@@ -12,33 +13,79 @@ class RestoreView extends GetView<BackupRestoreController> {
       init: controller,
       id: controller.restoreSectionId,
       builder: (_) {
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            "Restore".text.bold.size(22).make(),
-            verSpacing20,
-            GetBuilder(
-              id: controller.restoreTimeId,
-              init: controller,
-              builder: (_) {
-                return "Last Restore: ${controller.lastRestoreTime}"
+        return Form(
+          key: controller.formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              "Restore".text.bold.size(22).make(),
+              verSpacing20,
+              if (!controller.showRestoreInfo)
+                GetBuilder(
+                  id: controller.restoreTimeId,
+                  init: controller,
+                  builder: (_) {
+                    return "Last Restore: ${controller.lastRestoreTime}"
+                        .text
+                        .bold
+                        .size(16)
+                        .make();
+                  },
+                ),
+              if (controller.showRestoreInfo) ...[
+                "Total Passwords: ${controller.totalPasswords}"
                     .text
                     .bold
                     .size(16)
-                    .make();
-              },
-            ),
-            if (controller.showRestoreInfo)
-              "Total Passwords: ${controller.totalPasswords}"
-                  .text
-                  .bold
-                  .size(16)
-                  .make(),
-            verSpacing20,
-            if (!controller.showRestoreInfo)
-              _RestoreFromFileButton(controller: controller),
-          ],
-        ).box.p16.border(color: Colors.grey.shade700).roundedSM.make();
+                    .make(),
+                verSpacing8,
+                "Backup Date: ${controller.restoreFileBackupDate}"
+                    .text
+                    .bold
+                    .size(16)
+                    .make(),
+                verSpacing8,
+                Obx(
+                  () => PasswordWidget(
+                    hint: "Enter Password",
+                    focusNode: controller.focusNode,
+                    isPassObscure: controller.isPassObscure,
+                    togglePassVisibility: controller.togglePassVisibility,
+                    controller: controller.passController,
+                  ),
+                ),
+              ],
+              verSpacing20,
+              if (!controller.showRestoreInfo)
+                _RestoreFromFileButton(controller: controller),
+              if (controller.showRestoreInfo)
+                _RestoreButton(controller: controller),
+            ],
+          ).box.p16.border(color: Colors.grey.shade700).roundedSM.make(),
+        );
+      },
+    );
+  }
+}
+
+class _RestoreButton extends StatelessWidget {
+  const _RestoreButton({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
+
+  final BackupRestoreController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder(
+      id: controller.restoreButtonId,
+      init: controller,
+      builder: (_) {
+        return CustomButton(
+          "Start Restore",
+          onTap: controller.performRestore,
+        );
       },
     );
   }
@@ -56,7 +103,7 @@ class _RestoreFromFileButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return CustomButton(
       "Restore From File",
-      onTap: controller.performRestore,
+      onTap: controller.restoreFromFile,
     );
   }
 }
