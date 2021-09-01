6dd917297fd6714536cd8048a78c8722156af477
Smile Garg <smlgarg123@gmail.com>
2021-09-01 15:58:02 +0530

    change master password done

--- lib/app/modules/change_master_password/controllers/change_master_password_controller.dart
+++ lib/app/modules/change_master_password/controllers/change_master_password_controller.dart
@@ -1,20 +1,89 @@
+import 'package:custom_utils/log_utils.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:password_manager/app/core/utils/helpers.dart';
+import 'package:password_manager/app/core/values/strings.dart';
+import 'package:password_manager/app/data/services/database_service/database_service.dart';
+import 'package:password_manager/app/data/services/encryption_service.dart';
+import 'package:password_manager/app/data/services/secure_key_service.dart';
+import 'package:password_manager/app/modules/home/controllers/home_controller.dart';
 
 class ChangeMasterPasswordController extends GetxController {
-  //TODO: Implement ChangeMasterPasswordController
+  late final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  late final newPassController = TextEditingController();
+  late final confirmNewPassController = TextEditingController();
+  late final oldPassController = TextEditingController();
+
+  late final _isOldPassObscure = true.obs;
+  late final _isNewPassObscure = true.obs;
+  late final _isConfirmNewPassObscure = true.obs;
+
+  final wrongPassErrorMsg = "Old Password Is Wrong";
+  final changePassFailedErrorMsg = "Failed To Set New Password";
+  final dbErrorMsg = "Failed To Encrypt N Save Password To Database";
+  final successMsg = "Password Changed Successfully!!";
+
+  bool isLoading = false;
+
+  bool get isOldPassObscure => _isOldPassObscure.value;
+  bool get isNewPassObscure => _isNewPassObscure.value;
+  bool get isConfirmNewPassObscure => _isConfirmNewPassObscure.value;
+
+  void setPassword() async {
+    if (!formKey.currentState!.validate()) return;
+
+    toggleLoading(true);
+
+    final oldPass = oldPassController.text;
+    final newPass = newPassController.text;
+
+    final keyService = Get.find<SecureKeyService>();
+    final matchKey = await keyService.matchKey(oldPass, passwordKey);
+
+    if (!matchKey) {
+      toggleLoading(false);
+      return errorSnackbar(wrongPassErrorMsg);
+    }
+
+    final v = await keyService.saveKey(newPass, passwordKey);
+    if (!v) {
+      toggleLoading(false);
+      return errorSnackbar(changePassFailedErrorMsg);
+    }
+
+    /// Encrypting New Passwords
+    final encryService = Get.find<EncryptionService>();
+    final dbService = Get.find<DatabaseService>();
+    try {
+      Get.find<HomeController>().passwords.forEach((e) async {
+        e.password = encryService.decryptNEncrypt(e.password!, oldPass);
+        await dbService.connection.updatePass(e);
+      });
+    } on Exception catch (e, s) {
+      customLog("Db Updation", name: "Error", error: e, stackTrace: s);
+      errorSnackbar(dbErrorMsg);
+    }
+
+    Get.back();
+    successSnackbar(successMsg);
+    toggleLoading(false);
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  void toggleLoading(bool value) {
+    isLoading = value;
+    update();
   }
 
-  @override
-  void onClose() {}
-  void increment() => count.value++;
+  void toggleConfirmNewPassVisibility() {
+    _isConfirmNewPassObscure.value = !_isConfirmNewPassObscure.value;
+  }
+
+  void toggleNewPassVisibility() {
+    _isNewPassObscure.value = !_isNewPassObscure.value;
+  }
+
+  void toggleOldPassVisibility() {
+    _isOldPassObscure.value = !_isOldPassObscure.value;
+  }
 }
--- lib/app/modules/change_master_password/views/change_master_password_view.dart
+++ lib/app/modules/change_master_password/views/change_master_password_view.dart
@@ -1,22 +1,112 @@
+import 'package:custom_utils/spacing_utils.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-
+import 'package:password_manager/app/core/utils/helpers.dart';
+import 'package:password_manager/app/core/values/sizing.dart';
+import 'package:password_manager/app/global_widgets/auth_views_widgets.dart';
+import 'package:password_manager/app/global_widgets/buttons.dart';
+import 'package:velocity_x/velocity_x.dart';
 import '../controllers/change_master_password_controller.dart';
 
 class ChangeMasterPasswordView extends GetView<ChangeMasterPasswordController> {
+  final heading = "Change Password";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('ChangeMasterPasswordView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'ChangeMasterPasswordView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: Container(
+                width: context.width - 80,
+                child: Form(
+                  key: controller.formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      heading.text.size(20).bold.make(),
+                      verSpacing24,
+                      Obx(
+                        () => PasswordWidget(
+                          hint: 'Old Password',
+                          controller: controller.oldPassController,
+                          isPassObscure: controller.isOldPassObscure,
+                          togglePassVisibility:
+                              controller.toggleOldPassVisibility,
+                          validator: (v) {
+                            return passValidator(v, v, 8);
+                          },
+                        ),
+                      ),
+                      verSpacing16,
+                      Obx(
+                        () => PasswordWidget(
+                          hint: 'New Password',
+                          controller: controller.newPassController,
+                          isPassObscure: controller.isNewPassObscure,
+                          togglePassVisibility:
+                              controller.toggleNewPassVisibility,
+                          validator: (v) {
+                            final v2 = controller.confirmNewPassController.text;
+                            return passValidator(v, v2, 8);
+                          },
+                        ),
+                      ),
+                      verSpacing16,
+                      Obx(
+                        () => PasswordWidget(
+                          hint: 'Confirm New Password',
+                          controller: controller.confirmNewPassController,
+                          isPassObscure: controller.isConfirmNewPassObscure,
+                          togglePassVisibility:
+                              controller.toggleConfirmNewPassVisibility,
+                          validator: (v) {
+                            final v2 = controller.newPassController.text;
+                            return passValidator(v, v2, 8);
+                          },
+                        ),
+                      ),
+                      verSpacing32,
+                      GetBuilder(
+                        init: controller,
+                        builder: (_) {
+                          return CustomButton(
+                            "Submit",
+                            onTap: controller.setPassword,
+                            isLoading: controller.isLoading,
+                          );
+                        },
+                      )
+                    ],
+                  ),
+                ),
+              )
+                  .box
+                  .p16
+                  .color(Theme.of(context).backgroundColor)
+                  .withRounded(value: Sizing.radiusM)
+                  .shadowSm
+                  .make(),
+            ),
+          ),
+          Container(
+            width: context.width - 80,
+            child:
+                "Current Passwords Will Be Encrypted Using New Password. This May Take A While"
+                    .text
+                    .make(),
+          )
+              .box
+              .p16
+              .color(Theme.of(context).backgroundColor)
+              .withRounded(value: Sizing.radiusM)
+              .shadowSm
+              .make(),
+          verSpacing20
+        ],
       ),
     );
   }
